8488922b10827711ae0c3c576e11efeee7917baf
Matthew Koussa <matthew.koussa@gmail.com>
2019-01-11 00:24:08 -0600

    Finished knob label for knob names, not parameters.

--- displayLabels.js
+++ displayLabels.js
@@ -17,21 +17,51 @@ function displayLabels(){
     let vcoSustainLabel = text('Sustain', 628, vcoLabelsHeight);
     let vcoReleaseLabel = text('Release', 725, vcoLabelsHeight);
 
+    let lfoLabelHeight = 289
+    let lfoShapeLabel = text('Shape', 133, lfoLabelHeight);
+    let lfoRateLabel = text('Rate', 237, lfoLabelHeight);
+    let lfoIntensityLabel = text('Intensity', 329, lfoLabelHeight);
+    let lfoRangeLabel = text('Range', 433, lfoLabelHeight);
+
+    let filterLabelHeight = 400;
+    let filterEGAmountLabel = text('EG Amount', 67, filterLabelHeight);
+    let filterAttackLabel = text('Attack', 182, filterLabelHeight);
+    let filterDecayLabel = text('Decay', 284, filterLabelHeight);
+    let filterSustainLabel = text('Sustain', 378, filterLabelHeight);
+    let filterReleaseLabel = text('Release', 477, filterLabelHeight);
+    let filterReasonenceLabel = text('Reasonence', 564, filterLabelHeight);
 
     //Medium Labels
     textSize(20);
+    let filterCutoffLabel = text('Cutoff', 573, 274);
     //Vertical Labels
     rotate(-PI/2);
-    text("VCO 1", -140, 50);
-    text("VCO 2", -250, 50);
+    text('VCO 1', -140, 50);
+    text('VCO 2', -250, 50);
     rotate(PI/2);
 
 
     //Large Labels
     textSize(35);
     let oscMainLabel = text('Oscillators', 120 ,45);
-    let egMainLabel = text('Envelope Generator', 450, 45);
+    let egMainLabel = text('Envelope Generators', 435, 45);
 
+    //Vertical Labels
+    rotate(-PI/2);
+    text('LFO', -362, 90);
+    rotate(PI/2);
+
+    //Right Verical Labels
+    rotate(PI/2);
+    text('Filter', 340, -660);
+    rotate(-PI/2);
+
+    //Extra Large Labels
+    textSize(65);
+    //Vertical Labels
+    rotate(PI/2);
+    text('Blorp', 300, -730);
+    rotate(-PI/2);
 
 
 
