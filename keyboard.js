86056697165532196211697f2c2e57701859977c
Matthew Koussa <matthew.koussa@gmail.com>
2019-01-28 23:51:10 -0600

    Made the keys play wonky notes! Hurray!

--- keyboard.js
+++ keyboard.js
@@ -1,18 +1,25 @@
 
-keyPressed = function(mouseX, mouseY){
-  if(mouseX > 50 && mouseX < 90 && mouseY < 623){
-    osc1.start();
-  }
+keyPressed = function(mouseX, mouseY, keyboardPositionRightPressedVar){
+  let majorKeyWidth = 40;
+  for(keyPressedMajorKeyIterator = 0; keyPressedMajorKeyIterator < 14; keyPressedMajorKeyIterator++){
+    if(mouseX > (majorKeyWidth * keyPressedMajorKeyIterator + keyboardPositionRightPressedVar)
+      && mouseX < majorKeyWidth * (keyPressedMajorKeyIterator + 1) + keyboardPositionRightPressedVar
+      && mouseY < 623){
+      osc1.freq(441 + 16 * keyPressedMajorKeyIterator);
+      osc1Env.play();
+    }
+}
+                  
 }
 
-drawKeyboard = function(keyboardPositionDown, keyboardPositionRight) {
+drawKeyboard = function(keyboardPositionDown, keyboardPositionRightDrawVar) {
   fill(255);//Make the keys white
   let majorKeyWidth = 40;
   let majorKeyHeight = 133;
 
   let majorKeyIteration;
   for (majorKeyIteration = 0; majorKeyIteration < 14; majorKeyIteration++){
-    rect((majorKeyWidth * majorKeyIteration) + keyboardPositionRight, keyboardPositionDown, majorKeyWidth, majorKeyHeight);
+    rect((majorKeyWidth * majorKeyIteration) + keyboardPositionRightDrawVar, keyboardPositionDown, majorKeyWidth, majorKeyHeight);
   }
 
   fill(0);//Make the keys black
--- sketch.js
+++ sketch.js
@@ -1,41 +1,54 @@
-  var osc1;
-  var vco1VolumeKnob;
-
-function setup(){
-  createCanvas(windowWidth, windowHeight);
-  background('#ffd8d4');
-
-  osc1 = new p5.Oscillator();
-  osc1.setType('sine');
-  osc1.freq(440);
-  osc1.amp(0.5, 0.05);
-  osc1.attackTime = 50;
-  //osc1.start();
-
-  osc2 = new p5.Oscillator();
-  osc2.setType('square');
-  osc2.freq(441);
-  osc2.amp(20);
-  //osc2.start();
-
-  vco1VolumeKnob = new Knob(100, 110, 50);
-  //osc3 = new Oscillator(440);
-
-}
-
-function draw(){
+var osc1;
+var vco1VolumeKnob;
+var osc1Env;
+//Set Keyboard position
+let keyboardPositionRight = 70;
+let keyboardPositionDown = 490;
+  
+  function setup(){
+    createCanvas(windowWidth, windowHeight);
+    background('#ffd8d4');
+    
+    osc1 = new p5.Oscillator();
+    osc1.setType('sine');
+    osc1.freq(440);
+    let osc1AttackLevel = 0.5;
+    let osc1ReleaseLevel = 0;
+    let osc1Attack = 0.2;
+    let osc1Decay = 0.2;
+    let osc1Sustain = 0.2;
+    let osc1Release = 0.5;
+    osc1Env = new p5.Envelope();
+    osc1Env.setADSR(osc1Attack, osc1Decay, osc1Sustain, osc1Release);
+    osc1Env.setRange(osc1AttackLevel, osc1ReleaseLevel);
+    osc1.amp(osc1Env);
+    osc1.start();
+    
+    osc2 = new p5.Oscillator();
+    osc2.setType('square');
+    osc2.freq(441);
+    osc2.amp(20);
+    //osc2.start();
+    
+    vco1VolumeKnob = new Knob(100, 110, 50);
+    //osc3 = new Oscillator(440);
+    
+  }
+  
+  function draw(){
   displayLabels();
   drawKnobs();
-  drawKeyboard(490, 70);
+  drawKeyboard(keyboardPositionDown, keyboardPositionRight);
 }
 
 function mousePressed() {
-  if(mouseY > 490){
-    keyPressed(mouseX, mouseY);
+  if(mouseY > keyboardPositionDown){
+    keyPressed(mouseX, mouseY, keyboardPositionRight);
   }
 }
 
 function mouseReleased(){
-  osc1.stop();
+  //osc1.stop();
   osc2.stop();
 }
+ 
